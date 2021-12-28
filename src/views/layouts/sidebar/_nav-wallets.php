5790efc2f4cb2c8f76220cb804b26b80083a57b4
TimK <bootstrapbandit7@gmail.com>
2021-12-28 09:40:21 -0500

    remove loop out for now

--- src/views/layouts/sidebar/_nav-wallets.php
+++ src/views/layouts/sidebar/_nav-wallets.php
@@ -5,7 +5,7 @@
         <h2 class="text-center"><?=$wallet->user_label;?></h2>
         <br/>
         <?=\yii\helpers\Html::a('<img src="/img/icons/wallet.svg" style="height: 25px" /> Send/Receive/Transfer',['wallet/view','id'=>$wallet->external_hash],['class'=>'sidebar-item']);?>
-        <?=\yii\helpers\Html::a('<img src="/img/icons/loop.svg" style="height: 25px" /> Loop Out',['wallet/loop','id'=>$wallet->external_hash],['class'=>'sidebar-item']);?>
+        <?php //\yii\helpers\Html::a('<img src="/img/icons/loop.svg" style="height: 25px" /> Loop Out',['wallet/loop','id'=>$wallet->external_hash],['class'=>'sidebar-item']);?>
         <?=\yii\helpers\Html::a('<img src="/img/icons/rpc-forwarder.svg" style="height: 25px" />Keysend',['wallet/keysend','id'=>$wallet->external_hash],['class'=>'sidebar-item']);?>
         <?=\yii\helpers\Html::a('<img src="/img/icons/keys-auth.svg" style="height: 25px" />Access Keys',['wallet/access-keys','id'=>$wallet->external_hash],['class'=>'sidebar-item']);?>
         <?=\yii\helpers\Html::a('<img src="/img/icons/events.svg" style="height: 25px" /> Transactions',['wallet/transactions','id'=>$wallet->external_hash],['class'=>'sidebar-item']);?>
