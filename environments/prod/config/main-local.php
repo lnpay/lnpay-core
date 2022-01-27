8689c5562d055e2d0f36e693b41dd992d1df02c5
TimK <bootstrapbandit7@gmail.com>
2022-01-27 14:40:13 -0500

    add logging to environment

--- environments/prod/config/main-local.php
+++ environments/prod/config/main-local.php
@@ -2,6 +2,34 @@
 
 return [
     'components' => [
+        'log' => [
+            'traceLevel' => 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\DbTarget',
+                    'levels' => ['error','warning','info'],
+                    'logTable'=>'log',
+                    'except' => [
+                        'yii\web\HttpException:404',
+                        'yii\rbac\DbManager:*',
+                        'yii\db\*',
+                        'yii\web\Session::*',
+                        'yii\filters\RateLimiter::*',
+                        'yii\web\User::login',
+                        'yii\web\User::loginByCookie'
+                    ],
+                    'maskVars'=>[
+                        '_SERVER.DEFAULT_EMAIL_USERNAME',
+                        '_SERVER.DEFAULT_EMAIL_PASSWORD',
+                        '_SERVER.AMPLITUDE_API_KEY',
+                        '_SERVER.DB_USER',
+                        '_SERVER.DB_PASS',
+                        '_SERVER.DB_HOST',
+                        '_SERVER.DB_DB',
+                    ]
+                ],
+            ],
+        ],
         'cache' => [
             'class' => \yii\redis\Cache::class,
             'redis' => [
